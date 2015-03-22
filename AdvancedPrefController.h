ffc10dad9816241a662e1becbb0e8081eab27811
chikorita157 <chikorita157@optonline.net>
2015-03-21 20:47:19 -0400

    2.2.5 - Add Kodi/Plex Theater JSON-RPC support

---
+++ AdvancedPrefController.h
@@ -0,0 +1,14 @@
+//
+//  AdvancedPrefController.h
+//  MAL Updater OS X
+//
+//  Created by Tail Red on 3/21/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+#import "MASPreferencesViewController.h"
+
+@interface AdvancedPrefController : NSViewController <MASPreferencesViewController>
+
+@end
