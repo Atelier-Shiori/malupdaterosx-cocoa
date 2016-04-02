dc6c9854db2307f3064391615308a57a77b70cbe
アナスタシア <chikorita157>
2016-04-02 09:50:57 -0400

    2.2.9 - Update Sparkle to latest version, point Appcast URL to use https, add Safari Technology Preview support for stream detection

--- Utility.m
+++ Utility.m
@@ -92,7 +92,7 @@
         [alert addButtonWithTitle:@"Donate"];
         [alert addButtonWithTitle:@"Remind Me Later"];
         [alert setMessageText:@"Please Support MAL Updater OS X"];
-        [alert setInformativeText:@"We noticed that you have been using MAL Updater OS X for a while. Although MAL Updater OS X is free, it cost us money and time to develop this program and keep the Unofficial MAL API running. \r\rIf you find this program helpful, please consider making a donation. Funding will help future development and keep the program functional. Note that donating is completely optional."];
+        [alert setInformativeText:@"We noticed that you have been using MAL Updater OS X for a while. Although MAL Updater OS X is free, it cost us money and time to develop this program as I don't have as much free time to work on it. \r\rIf you find this program helpful, please consider making a donation. Funding will help future development and keep the program functional. Note that donating is completely optional."];
         [alert setShowsSuppressionButton:YES];
         // Set Message type to Warning
         [alert setAlertStyle:NSInformationalAlertStyle];
