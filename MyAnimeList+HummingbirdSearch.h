acaa6ad67ad69f22f1832daef488633f51d44850
アナスタシア <chikorita157>
2016-04-13 16:25:01 -0400

    2.2.10 - Add ability to use Hummngbird.me Search API instead of Atarashii-API for more accurate matches. Also, fix the total episodes display if there is none.

---
+++ MyAnimeList+HummingbirdSearch.h
@@ -0,0 +1,13 @@
+//
+//  MyAnimeList+HummingbirdSearch.h
+//  MAL Updater OS X
+//
+//  Created by アナスタシア on 2016/04/13.
+//
+//
+
+#import "MyAnimeList.h"
+
+@interface MyAnimeList (HummingbirdSearch)
+
+@end
