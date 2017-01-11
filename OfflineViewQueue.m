c5d65f276ee56f90c6567f4a03f178e7748c4ea6
桐間紗路 <kirimasharo@kirikansharos-mac-pro.local>
2017-01-10 21:44:34 -0500

    2.3 - Add ability to clear offline queue

--- OfflineViewQueue.m
+++ OfflineViewQueue.m
@@ -29,5 +29,44 @@
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
 }
+-(IBAction)clearqueue:(id)sender
+{
+    // Set Up Prompt Message Window
+    NSAlert * alert = [[NSAlert alloc] init];
+    [alert addButtonWithTitle:@"Yes"];
+    [alert addButtonWithTitle:@"No"];
+    [alert setMessageText:@"Are you sure you want to clear the Offline Queue?"];
+    [alert setInformativeText:@"Once done, this action cannot be undone."];
+    // Set Message type to Warning
+    [alert setAlertStyle:NSWarningAlertStyle];
+    // Show as Sheet on historywindow
+    [alert beginSheetModalForWindow:self.window
+                      modalDelegate:self
+                     didEndSelector:@selector(clearhistoryended:code:conext:)
+                        contextInfo:NULL];
+    
+}
+-(void)clearhistoryended:(NSAlert *)alert
+                    code:(int)echoice
+                  conext:(void *)v
+{
+    if (echoice == 1000) {
+        // Remove All Data
+        NSManagedObjectContext *moc = [self managedObjectContext];
+        NSFetchRequest * allQueue = [[NSFetchRequest alloc] init];
+        [allQueue setEntity:[NSEntityDescription entityForName:@"OfflineQueue" inManagedObjectContext:moc]];
+        
+        NSError * error = nil;
+        NSArray * queue = [moc executeFetchRequest:allQueue error:&error];
+        //error handling goes here
+        for (NSManagedObject * item in queue) {
+            [moc deleteObject:item];
+        }
+        [moc save:&error];
+        // Clear Core Data Objects from Memory
+        [moc reset];
+    }
+    
+}
 
 @end
