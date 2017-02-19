fc196fa412712c8b5c8e453e8ac9b9aab35a6ca0
桐間紗路 <kirimasharo@tongjianshalus-MacBook-Pro.local>
2017-02-19 09:16:15 -0800

    2.3 - UI tweaks, add copyright dates to new source files.

--- DonationWindowController.h
+++ DonationWindowController.h
@@ -3,7 +3,7 @@
 //  MAL Updater OS X
 //
 //  Created by 桐間紗路 on 2017/01/03.
-//
+//  Copyright 2009-2017 Atelier Shiori. All rights reserved. Code licensed under New BSD License
 //
 
 #import <Cocoa/Cocoa.h>
