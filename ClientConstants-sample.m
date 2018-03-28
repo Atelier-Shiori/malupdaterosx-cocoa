e2a73f80c0363fae0fe57726290b534a1bf7c84d
chikorita157 <chikorita157@chikorita157.com>
2018-03-28 18:57:11 -0400

    2.5 - Stage the groundwork

---
+++ ClientConstants-sample.m
@@ -0,0 +1,13 @@
+//
+//  ClientConstants.m
+//  MAL Updater OS X
+//
+//  Created by 小鳥遊六花 on 3/28/18.
+//
+
+#import "ClientConstants.h"
+
+@implementation ClientConstants
+// Specify MAL API client ID below
+NSString *const kMALClientID = @"":
+@end
---
+++ ClientConstants.h
@@ -0,0 +1,12 @@
+//
+//  ClientConstants.h
+//  MAL Updater OS X
+//
+//  Created by 小鳥遊六花 on 3/28/18.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ClientConstants : NSObject
+FOUNDATION_EXPORT NSString *const kMALClientID;
+@end
